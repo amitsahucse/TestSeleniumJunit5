e43c234753293fad9d6db580c4ed506d5e29b87c
amitsahu <amit-kumar.sahu@lhind.dlh.de>
2020-01-08 10:59:26 +0100

    test

---
+++ src/main/java/SeleniumProject/TestSeleniumJunit5/App.java
@@ -0,0 +1,13 @@
+package SeleniumProject.TestSeleniumJunit5;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
